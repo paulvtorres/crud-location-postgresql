8c88c43cd116a916b989dfa170cfa5b8a7833493
paulvtorres <87663669+paulvtorres@users.noreply.github.com>
2024-06-01 22:13:03 -0500

    Fix log to insert

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -47,7 +47,7 @@ const saveLocation = async (req,res)=>{
     const lng = req.body.lng;
     const query = "INSERT INTO location (phoneNum,name,lastName,lat,lng,lastDate) VALUES ($1,'','',$2,$3,to_char(NOW() - interval '5 hour', 'YYYY-MM-DD HH24:MI:SS')) ON CONFLICT (phoneNum) DO UPDATE SET lat = $2, lng = $3, lastDate = to_char(NOW() - interval '5 hour', 'YYYY-MM-DD HH24:MI:SS')"
     const response = await pool.query(query,[phoneNum, lat,lng]);
-    console.log(query)
+    console.log("$phoneNum OK") 
     const responseData = {
         success: true,
         data: [],
