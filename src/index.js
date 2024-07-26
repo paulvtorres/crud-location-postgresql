4de424716af85d312bd7231b0bd6347447543060
Paul Torres <87663669+paulvtorres@users.noreply.github.com>
2024-07-26 14:26:16 -0500

    Add files via upload
    return 0

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended:false}));
 //routes
 app.use(require('./routes/index'));
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 1;
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 app.listen(process.env.PORT||4000);
 
